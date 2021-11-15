a95cd9ec51686ff16f86aa790442ad0579e89341
Yoongyeong Song <14dnfnfn@gmail.com>
2021-11-15 17:04:52 +0900

    #7 [style] splash screen

---
+++ app/src/main/java/com/pns/albang/LaunchActivity.kt
@@ -0,0 +1,14 @@
+package com.pns.albang
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LaunchActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        startActivity(Intent(applicationContext, MainActivity::class.java))
+        finish()
+    }
+}
