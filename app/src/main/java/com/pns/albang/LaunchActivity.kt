282add48409aaf422d921cc493f2837aa7c8f118
leesh96 <suho2718@gmail.com>
2021-11-16 13:28:19 +0900

    #22 [feature] : location, camera permission request at launch
    tedpermisson 라이브러리 사용 권한 체크
    manifest에 명시적 권한 부여
    필요한 string resource 추가

--- app/src/main/java/com/pns/albang/LaunchActivity.kt
+++ app/src/main/java/com/pns/albang/LaunchActivity.kt
@@ -1,14 +1,75 @@
 package com.pns.albang
 
+import android.Manifest
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
 
-class LaunchActivity: AppCompatActivity() {
+class LaunchActivity : AppCompatActivity() {
+
+    private val permissionListener = object : PermissionListener {
+        override fun onPermissionGranted() {
+            Log.d(TAG, "permission granted")
+            navigateActivity("main")
+        }
+
+        override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+            showAlertDialog("permission denied")
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        startActivity(Intent(applicationContext, MainActivity::class.java))
-        finish()
+        checkPermission(permissionListener)
+    }
+
+    private fun checkPermission(permissionListener: PermissionListener) =
+        TedPermission.create()
+            .setPermissionListener(permissionListener)
+            .setRationaleTitle(getString(R.string.permission_rational_title))
+            .setRationaleMessage(getString(R.string.permission_rational_message))
+            .setRationaleConfirmText(getString(R.string.permission_rational_button))
+            .setDeniedTitle(getString(R.string.permission_denied_title))
+            .setDeniedMessage(getString(R.string.permission_denied_message))
+            .setDeniedCloseButtonText(getString(R.string.btn_confirm))
+            .setGotoSettingButton(false)
+            .setPermissions(FINE_LOCATION, COARSE_LOCATION, CAMERA)
+            .check()
+
+    private fun navigateActivity(where: String) {
+        when (where) {
+            "main" -> {
+                startActivity(Intent(this@LaunchActivity, MainActivity::class.java))
+                finish()
+            }
+        }
+    }
+
+    private fun showAlertDialog(type: String) {
+        when (type) {
+            "permission denied" -> {
+                MaterialAlertDialogBuilder(this@LaunchActivity)
+                    .setTitle(getString(R.string.permission_denied_dialog_message))
+                    .setPositiveButton(getString(R.string.permission_denide_dialog_button)) { dialogInterface, _ ->
+                        dialogInterface.dismiss()
+                        finish()
+                    }
+                    .setCancelable(false)
+                    .show()
+            }
+        }
+    }
+
+    companion object {
+        private const val TAG = "LAUNCH ACTIVITY"
+
+        private const val FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION
+        private const val COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION
+        private const val CAMERA = Manifest.permission.CAMERA
     }
 }
