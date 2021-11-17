0ca114036a9518328becffb342a590d06db61fdd
Yoongyeong Song <14dnfnfn@gmail.com>
2021-11-17 23:44:36 +0900

    #30 [feature] recyclerview adapter create

--- app/src/main/java/com/pns/albang/ReviewActivity.kt
+++ app/src/main/java/com/pns/albang/ReviewActivity.kt
@@ -3,27 +3,42 @@ package com.pns.albang
 import android.animation.ObjectAnimator
 import android.content.res.ColorStateList
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.pns.albang.databinding.ActivityReviewBinding
 
 class ReviewActivity : AppCompatActivity() {
     private lateinit var binding: ActivityReviewBinding
+    private lateinit var reviewAdapter: ReviewAdapter
     private var isFabOpen = false
+
+    private var testList = mutableListOf(Review("1", "내가 쓴 방명록", true, false),
+        Review("2", "신고하지 않은 방명록", false, false),
+        Review("3", "신고완료 한 방명록", false, true))
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityReviewBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.heritageName = "sample text"
+        binding.heritageName = "경복궁"
 
         binding.fabReview.setOnClickListener {
             toggleFab()
         }
 
+        settingRecyclerview()
+    }
+
+    private fun settingRecyclerview() {
+        reviewAdapter = ReviewAdapter()
+        binding.rcReview.apply {
+            layoutManager = LinearLayoutManager(applicationContext)
+            adapter = reviewAdapter
+        }
+        reviewAdapter.submitList(testList)
 
     }
 
@@ -34,17 +49,19 @@ class ReviewActivity : AppCompatActivity() {
             ObjectAnimator.ofFloat(binding.fabAdd, "translationY", 0f).apply { start() }
             ObjectAnimator.ofFloat(binding.fabAr, "translationY", 0f).apply { start() }
             ObjectAnimator.ofFloat(binding.fabRefresh, "translationY", 0f).apply { start() }
-            binding.fabReview.backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(applicationContext, R.color.colorSecondary))
+            binding.fabReview.backgroundTintList =
+                ColorStateList.valueOf(ContextCompat.getColor(applicationContext, R.color.colorSecondary))
             ObjectAnimator.ofFloat(binding.fabReview, View.ROTATION, 45f, 0f).apply { start() }
-            binding.fabReview.setImageDrawable(ContextCompat.getDrawable(applicationContext,R.drawable.fab_review_ar))
+            binding.fabReview.setImageDrawable(ContextCompat.getDrawable(applicationContext, R.drawable.fab_review_ar))
         } else {
             binding.fabBackground.visibility = View.VISIBLE
             binding.motionLayout.isInteractionEnabled = false
             ObjectAnimator.ofFloat(binding.fabAdd, "translationY", -250f).apply { start() }
             ObjectAnimator.ofFloat(binding.fabAr, "translationY", -500f).apply { start() }
             ObjectAnimator.ofFloat(binding.fabRefresh, "translationY", -750f).apply { start() }
-            binding.fabReview.backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(applicationContext, R.color.colorPrimary))
-            binding.fabReview.setImageDrawable(ContextCompat.getDrawable(applicationContext,R.drawable.fab_review_add))
+            binding.fabReview.backgroundTintList =
+                ColorStateList.valueOf(ContextCompat.getColor(applicationContext, R.color.colorPrimary))
+            binding.fabReview.setImageDrawable(ContextCompat.getDrawable(applicationContext, R.drawable.fab_review_add))
             ObjectAnimator.ofFloat(binding.fabReview, View.ROTATION, 0f, 45f).apply { start() }
         }
         isFabOpen = !isFabOpen
---
+++ app/src/main/java/com/pns/albang/ReviewAdapter.kt
@@ -0,0 +1,37 @@
+package com.pns.albang
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.pns.albang.databinding.ItemReviewBinding
+
+class ReviewAdapter : ListAdapter<Review, ReviewAdapter.ReviewViewHolder>(diffUtil) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReviewViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = ItemReviewBinding.inflate(layoutInflater, parent, false)
+        return ReviewViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ReviewViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    inner class ReviewViewHolder(private val binding: ItemReviewBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(review: Review) {
+            binding.review = review
+        }
+    }
+
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<Review>() {
+            override fun areContentsTheSame(oldItem: Review, newItem: Review) =
+                oldItem == newItem
+
+            override fun areItemsTheSame(oldItem: Review, newItem: Review) =
+                oldItem.reviewId == newItem.reviewId
+        }
+    }
+}
