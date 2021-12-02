f3014cfa6784e3e9c18437c7ca06fed43f971906
Lee Suho <suho2718@gmail.com>
2021-12-02 20:52:11 +0900

    #44 [feature] : MainViewModel 개발
    랜드마크 데이터 가져와서 뷰에서 사용할 데이터 모델로 매핑
    반경내의 랜드마크 찾는 로직 개발
    MainActivity에서 이벤트 처리

--- app/src/main/java/com/pns/albang/MainActivity.kt
+++ app/src/main/java/com/pns/albang/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.pns.albang
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.Gravity
-import android.view.WindowManager
-import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.CameraUpdate
 import com.naver.maps.map.LocationTrackingMode
@@ -13,22 +14,26 @@ import com.naver.maps.map.MapFragment
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
 import com.naver.maps.map.overlay.CircleOverlay
+import com.naver.maps.map.overlay.InfoWindow
+import com.naver.maps.map.overlay.Marker
 import com.naver.maps.map.overlay.OverlayImage
 import com.naver.maps.map.util.FusedLocationSource
 import com.pns.albang.databinding.ActivityMainBinding
+import com.pns.albang.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel by viewModels()
 
     private lateinit var naverMap: NaverMap
     private lateinit var fusedLocationSource: FusedLocationSource
     private lateinit var circleOverlay: CircleOverlay
+    private val landmarkInfoWindow = InfoWindow()
 
     private val onMapReadyCallback = OnMapReadyCallback {
         this.naverMap = it.apply {
             val uiSettings = this.uiSettings
-
             uiSettings.apply {
                 isCompassEnabled = false
                 isScaleBarEnabled = false
@@ -37,7 +42,6 @@ class MainActivity : AppCompatActivity() {
                 logoGravity = Gravity.TOP
                 logoGravity = Gravity.END
             }
-
             binding.mapLocation.map = this
             binding.mapZoomControl.map = this
 
@@ -48,6 +52,7 @@ class MainActivity : AppCompatActivity() {
 
             circleOverlay = CircleOverlay(locationOverlay.position, 50.0)
             circleOverlay.map = this
+
             locationOverlay.circleRadius = 0
             locationOverlay.icon = OverlayImage.fromResource(R.drawable.ic_map_location)
             locationOverlay.isVisible = true
@@ -58,8 +63,16 @@ class MainActivity : AppCompatActivity() {
                 circleOverlay.radius = 50.0
                 circleOverlay.outlineWidth = 0
                 circleOverlay.color = getColor(R.color.locationOverlayColor)
+
+                viewModel.checkInBoundLandmark(circleOverlay.bounds)
+            }
+
+            this.setOnMapClickListener { _, _ ->
+                landmarkInfoWindow.close()
             }
         }
+
+        setObserver()
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
@@ -67,10 +80,12 @@ class MainActivity : AppCompatActivity() {
             if (!fusedLocationSource.isActivated) {
                 naverMap.locationTrackingMode = LocationTrackingMode.None
             }
+
             return
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -83,8 +98,58 @@ class MainActivity : AppCompatActivity() {
         mapFragment.getMapAsync(onMapReadyCallback)
     }
 
+    private fun setObserver() {
+        viewModel.landmarks.observe(this) {
+            it.forEach { landmark ->
+                landmark.mapMarker.map = naverMap
+                landmarkInfoWindow.adapter = object : InfoWindow.DefaultTextAdapter(this@MainActivity) {
+                    override fun getText(infoWindow: InfoWindow): CharSequence {
+                        return infoWindow.marker?.tag as CharSequence? ?: ""
+                    }
+                }
+                landmark.mapMarker.setOnClickListener { overlay ->
+                    val marker = overlay as Marker
+
+                    if (marker.infoWindow == null) {
+                        landmarkInfoWindow.open(marker)
+                    } else {
+                        landmarkInfoWindow.close()
+                    }
+
+                    true
+                }
+            }
+        }
+
+        viewModel.targetLandmark.observe(this) {
+            val boundedLandmark = it.getContentIfNotHandled()
+            val intent = if (boundedLandmark == null) {
+                Intent(this, TestActivity::class.java)
+            } else {
+                Intent(this, ReviewActivity::class.java).apply {
+                    putExtra("landmark", boundedLandmark.id)
+                    putExtra("landmarkImage", boundedLandmark.imageName)
+                    putExtra("landmarkName", boundedLandmark.name)
+                }
+            }
+
+            if (boundedLandmark == null) {
+                binding.btnBottom.icon = ContextCompat.getDrawable(this, R.drawable.ic_btn_request)
+                binding.btnBottom.text = getString(R.string.btn_request)
+            } else {
+                binding.btnBottom.icon = ContextCompat.getDrawable(this, R.drawable.ic_guestbook)
+                binding.btnBottom.text = getString(R.string.btn_show_guestbook)
+            }
+
+            binding.btnBottom.setOnClickListener {
+                startActivity(intent)
+            }
+        }
+    }
+
     companion object {
         private const val TAG = "MAIN ACTIVITY"
+
         private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
     }
 }
